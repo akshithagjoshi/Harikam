21f7765c43e07a8e781f10de3f8d17c94de7eeee
akshithagjoshi <131952903+akshithagjoshi@users.noreply.github.com>
2023-05-08 02:18:03 -0300

    Add files via upload

---
+++ Exp2.java
@@ -0,0 +1,15 @@
+class Exp2
+{
+  public static void main (String s[])
+   {
+    int n =3;
+   if(n%2==0)
+      { 
+     System.out.println("even");
+       } 
+   else 
+     {
+     System.out.println("odd");
+      }
+    }
+}
